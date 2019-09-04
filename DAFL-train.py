4203bffd94e6679f7bbe09bb20c185a8e028e811
HantingChen <40243544+HantingChen@users.noreply.github.com>
2019-09-04 18:42:39 +0800

    Add files via upload

---
+++ DAFL-train.py
@@ -0,0 +1,204 @@
+#Copyright (C) 2019. Huawei Technologies Co., Ltd. All rights reserved.
+
+#This program is free software; you can redistribute it and/or modify it under the terms of the BSD 3-Clause License.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3-Clause License for more details.
+
+import argparse
+import os
+import numpy as np
+import math
+import sys
+import pdb
+
+import torchvision.transforms as transforms
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torchvision.datasets.mnist import MNIST
+from lenet import LeNet5Half
+from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR100
+import resnet
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='MNIST', choices=['MNIST','cifar10','cifar100'])
+parser.add_argument('--data', type=str, default='/cache/data/')
+parser.add_argument('--teacher_dir', type=str, default='/cache/models/')
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batch_size', type=int, default=512, help='size of the batches')
+parser.add_argument('--lr_G', type=float, default=0.2, help='learning rate')
+parser.add_argument('--lr_S', type=float, default=2e-3, help='learning rate')
+parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--channels', type=int, default=1, help='number of image channels')
+parser.add_argument('--oh', type=float, default=1, help='one hot loss')
+parser.add_argument('--ie', type=float, default=5, help='information entropy loss')
+parser.add_argument('--a', type=float, default=0.1, help='activation loss')
+parser.add_argument('--output_dir', type=str, default='/cache/models/')
+
+opt = parser.parse_args()
+
+img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+cuda = True 
+
+accr = 0
+accr_best = 0
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.init_size = opt.img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128*self.init_size**2))
+
+        self.conv_blocks0 = nn.Sequential(
+            nn.BatchNorm2d(128),
+        )
+        self.conv_blocks1 = nn.Sequential(
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.conv_blocks2 = nn.Sequential(
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
+            nn.Tanh(),
+            nn.BatchNorm2d(opt.channels, affine=False) 
+        )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks0(out)
+        img = nn.functional.interpolate(img,scale_factor=2)
+        img = self.conv_blocks1(img)
+        img = nn.functional.interpolate(img,scale_factor=2)
+        img = self.conv_blocks2(img)
+        return img
+        
+generator = Generator().cuda()
+    
+teacher = torch.load(opt.teacher_dir + 'teacher').cuda()
+teacher.eval()
+criterion = torch.nn.CrossEntropyLoss().cuda()
+
+teacher = nn.DataParallel(teacher)
+generator = nn.DataParallel(generator)
+
+def kdloss(y, teacher_scores):
+    p = F.log_softmax(y, dim=1)
+    q = F.softmax(teacher_scores, dim=1)
+    l_kl = F.kl_div(p, q, size_average=False)  / y.shape[0]
+    return l_kl
+
+if opt.dataset == 'MNIST':    
+    # Configure data loader   
+    net = LeNet5Half().cuda()
+    net = nn.DataParallel(net)
+    data_test = MNIST(opt.data,
+                      train=False,
+                      transform=transforms.Compose([
+                          transforms.Resize((32, 32)),
+                          transforms.ToTensor(),
+                          transforms.Normalize((0.1307,), (0.3081,))
+                          ]))           
+    data_test_loader = DataLoader(data_test, batch_size=64, num_workers=1, shuffle=False)
+
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr_G)
+    optimizer_S = torch.optim.Adam(net.parameters(), lr=opt.lr_S)
+
+if opt.dataset != 'MNIST':  
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    if opt.dataset == 'cifar10': 
+        net = resnet.ResNet18().cuda()
+        net = nn.DataParallel(net)
+        data_test = CIFAR10(opt.data,
+                          train=False,
+                          transform=transform_test)
+    if opt.dataset == 'cifar100': 
+        net = resnet.ResNet18(num_classes=100).cuda()
+        net = nn.DataParallel(net)
+        data_test = CIFAR100(opt.data,
+                          train=False,
+                          transform=transform_test)
+    data_test_loader = DataLoader(data_test, batch_size=opt.batch_size, num_workers=0)
+
+    # Optimizers
+    optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr_G)
+
+    optimizer_S = torch.optim.SGD(student.parameters(), lr=opt.lr_S, momentum=0.9, weight_decay=5e-4)
+
+
+def adjust_learning_rate(optimizer, epoch, learing_rate):
+    if epoch < 800:
+        lr = learing_rate
+    elif epoch < 1600:
+        lr = 0.1*learing_rate
+    else:
+        lr = 0.01*learing_rate
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+        
+        
+# ----------
+#  Training
+# ----------
+
+batches_done = 0
+for epoch in range(opt.n_epochs):
+
+    total_correct = 0
+    avg_loss = 0.0
+    if opt.dataset != 'MNIST':
+        adjust_learning_rate(optimizer_S, epoch, opt.lr_S)
+
+    for i in range(120):
+        net.train()
+        z = Variable(torch.randn(opt.batch_size, opt.latent_dim)).cuda()
+        optimizer_G.zero_grad()
+        optimizer_S.zero_grad()        
+        gen_imgs = generator(z)
+        outputs_T, features_T = teacher(gen_imgs, out_feature=True)   
+        pred = outputs_T.data.max(1)[1]
+        loss_activation = -features_T.abs().mean()
+        loss_one_hot = criterion(outputs_T,pred)
+        softmax_o_T = torch.nn.functional.softmax(outputs_T, dim = 1).mean(dim = 0)
+        loss_information_entropy = (softmax_o_T * torch.log10(softmax_o_T)).sum()
+        loss = loss_one_hot * opt.oh + loss_information_entropy * opt.ie + loss_activation * opt.a
+        loss_kd = kdloss(net(gen_imgs), outputs_T.detach()) 
+        loss += loss_kd       
+        loss.backward()
+        optimizer_G.step()
+        optimizer_S.step() 
+        if i == 1:
+            print ("[Epoch %d/%d] [loss_oh: %f] [loss_ie: %f] [loss_a: %f] [loss_kd: %f]" % (epoch, opt.n_epochs,loss_one_hot.item(), loss_information_entropy.item(), loss_activation.item(), loss_kd.item()))
+            
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(data_test_loader):
+            images = images.cuda()
+            labels = labels.cuda()
+            net.eval()
+            output = net(images)
+            avg_loss += criterion(output, labels).sum()
+            pred = output.data.max(1)[1]
+            total_correct += pred.eq(labels.data.view_as(pred)).sum()
+
+    avg_loss /= len(data_test)
+    print('Test Avg. Loss: %f, Accuracy: %f' % (avg_loss.data.item(), float(total_correct) / len(data_test)))
+    accr = round(float(total_correct) / len(data_test), 4)
+    if accr > accr_best:
+        torch.save(net,opt.output_dir + 'student')
+        accr_best = accr
---
+++ README.md
@@ -0,0 +1,36 @@
+# DAFL: Data-Free Learning of Student Networks
+This code is the implementation of ICCVL2019 paper [DAFL: Data-Free Learning of Student Networks](https://arxiv.org/pdf/1904.01186.pdf)
+
+## Run
+```python
+python teacher-train.py
+```
+First, you should train a teacher network.
+```python
+python DAFL-train.py
+```
+Then, you can use the DAFL to train a student network without training data on the MNIST dataset.
+
+To run DAFL on the CIFAR-10 dataset
+```python
+python teacher-train.py --dataset cifar10
+python DAFL-train.py --dataset cifar10 --channels 3 --n_epochs 2000 --batch_size 1024 --lr_G 0.02 --latent_dim 1000
+```
+
+To run DAFL on the CIFAR-100 dataset
+```python
+python teacher-train.py --dataset cifar100
+python DAFL-train.py --dataset cifar100 --channels 3 --n_epochs 2000 --batch_size 1024 --lr_G 0.02 --latent_dim 1000 --oh 0.5
+```
+
+## Citation
+	@inproceedings{DAFL,
+		title={DAFL: Data-Free Learning of Student Networks},
+		author={Chen, Hanting and Wang, Yunhe and Xu, Chang and Yang, Zhaohui and Liu, Chuanjian and Shi, Boxin and Xu, Chunjing and Xu, Chao and Tian, Qi},
+		booktitle={ICCV},
+		year={2019}
+	}
+### Contributing
+We appreciate all contributions. If you are planning to contribute back bug-fixes, please do so without any further discussion.
+
+If you plan to contribute new features, utility functions or extensions to the core, please first open an issue and discuss the feature with us. Sending a PR without discussion might end up resulting in a rejected PR, because we might be taking the core in a different direction than you might be aware of.
