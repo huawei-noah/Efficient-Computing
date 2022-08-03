5467e78944b92f9137e709631ca83ad33f9c063b
unknown <1471910080@qq.com>
2022-08-03 14:57:43 +0800

    quantization

---
+++ Quantization/DynamicQuant/resnet_quan_imagenet.py
@@ -0,0 +1,306 @@
+# 2022.07.19-Changed for implementation for DQ model
+#            Huawei Technologies Co., Ltd. <foss@huawei.com>
+"""
+This script was started from an early version of the PyTorch ImageNet example 
+(https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py)
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch.utils.model_zoo as model_zoo
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from utils.quan_conv import QuanConv
+from utils.quan_conv import DynamicQConv as MyConv
+
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+           'resnet152']
+
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+Conv = nn.Conv2d
+
+def conv3x3(in_planes, out_planes, name_w, name_a, nbit_w, nbit_a, stride=1):
+    "3x3 convolution with padding"
+    return MyConv(in_planes, out_planes, 3, name_w, name_a, nbit_w, nbit_a, stride=stride, padding=1, bias=False)
+
+
+class LambdaLayer(nn.Module):
+    def __init__(self, lambd):
+        super(LambdaLayer, self).__init__()
+        self.lambd = lambd
+
+    def forward(self, x):
+        return self.lambd(x)
+       
+        
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, name_w, name_a, nbit_w, nbit_a, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, name_w, name_a, nbit_w, nbit_a, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes, name_w, name_a, nbit_w, nbit_a)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, one_hot):
+        residual = x
+
+        out = self.conv1(x, one_hot)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out, one_hot)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample[0](x, one_hot)
+            residual = self.downsample[1](residual)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class BasicBlock_Quan(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, name_w, name_a, nbit_w, nbit_a, stride=1, downsample=None):
+        super(BasicBlock_Quan, self).__init__()
+        self.conv1 = QuanConv(inplanes, planes, 3, name_w, name_a, nbit_w, nbit_a, stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = QuanConv(planes, planes, 3, name_w, name_a, nbit_w, nbit_a, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample[0](x)
+            residual = self.downsample[1](residual)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, name_w, name_a, nbit_w, nbit_a, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = MyConv(inplanes, planes, name_w, name_a, nbit_w, nbit_a, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = MyConv(planes, planes, name_w, name_a, nbit_w, nbit_a, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = MyConv(planes, planes * 4, name_w, name_a, nbit_w, nbit_a, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x, one_hot):
+        residual = x
+
+        out = self.conv1(x, one_hot)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out, one_hot)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out, one_hot)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample[0](x, one_hot)
+            residual = self.downsample[1](residual)
+            # residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, name_w, name_a, nbit_w, nbit_a, num_bits=3, num_classes=1000, width=1.):
+        self.inplanes = int(64*width)
+        super(ResNet, self).__init__()
+        self.conv1 = Conv(3, int(64*width), kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(int(64*width))
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(BasicBlock_Quan, int(64*width),  layers[0], name_w, name_a, nbit_w, nbit_a)
+        self.layer2 = self._make_layer(block, int(128*width), layers[1], name_w, name_a, nbit_w, nbit_a, stride=2)
+        self.layer3 = self._make_layer(block, int(256*width), layers[2], name_w, name_a, nbit_w, nbit_a, stride=2)
+        self.layer4 = self._make_layer(block, int(512*width), layers[3], name_w, name_a, nbit_w, nbit_a, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(int(512*width) * block.expansion, num_classes)
+        
+        
+        self.avgpool_policy = nn.AvgPool2d((7,7))
+        self.fc1 = nn.Linear(64*8*8, 64)
+        self.dropout = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(64, num_bits)
+
+        for m in self.modules():
+            if isinstance(m, Conv):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, name_w, name_a, nbit_w, nbit_a, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.ModuleList()
+            downsample.append(MyConv(self.inplanes, planes * block.expansion, 
+                              1, name_w, name_a, nbit_w, nbit_a, stride=stride, bias=False))
+            downsample.append(nn.BatchNorm2d(planes * block.expansion))
+
+        layers = nn.ModuleList()
+        layers.append(block(self.inplanes, planes, name_w, name_a, nbit_w, nbit_a, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, name_w, name_a, nbit_w, nbit_a))
+
+        return layers #nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        for m in self.layer1:
+            x = m(x)
+        
+        feat = self.avgpool_policy(x)
+        feat = self.fc1(feat.view(x.size(0),-1))
+        feat = self.dropout(feat)
+        feat = self.fc2(feat)
+        one_hot = F.gumbel_softmax(feat, tau=1, hard=True, eps=1e-20)
+        
+        for m in self.layer2:
+            x = m(x, one_hot)
+        for m in self.layer3:
+            x = m(x, one_hot)
+        for m in self.layer4:
+            x = m(x, one_hot)
+
+        # x = self.layer1(x)
+        # x = self.layer2(x)
+        # x = self.layer3(x)
+        # x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x, one_hot
+
+
+def resnet18(pretrained=False, name_w='pact', name_a='pact', nbit_w=4, nbit_a=4, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], name_w, name_a, nbit_w, nbit_a, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+
+def resnet34(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet34']))
+    return model
+
+
+def resnet50(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
+    return model
+
+
+def resnet101(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
+    return model
+
+
+def resnet152(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
+    return model
+
+
+if __name__ == '__main__':
+    use_gpu = True
+    batch_size = 1
+    resolution = 1024
+    x = torch.randn(batch_size, 3, resolution, resolution)
+#     model = resnet18(width=1)
+    model = resnet50(width=1)
+    if use_gpu:
+        x = x.cuda()
+        model = model.cuda()
+    model.eval()
+    print(model)
---
+++ Quantization/DynamicQuant/utils/quan_conv.py
@@ -0,0 +1,262 @@
+# 2022.07.19-Implementation for building DQ model
+#            Huawei Technologies Co., Ltd. <foss@huawei.com>
+import math
+import time
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+import torch.nn.functional as F
+
+
+class Signer(Function):
+    '''
+    take a real value x
+    output sign(x)
+    '''
+    @staticmethod
+    def forward(ctx, input):
+#         return torch.sign(input)
+        return (input>=0).float()*2-1
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output
+
+
+def sign(input):
+    return Signer.apply(input)
+
+
+
+# quantize for weights and activations
+class Quantizer(Function):
+    '''
+    take a real value x in alpha*[0,1] or alpha*[-1,1]
+    output a discrete-valued x in alpha*{0, 1/(2^k-1), ..., (2^k-1)/(2^k-1)} or likeness
+    where k is nbit
+    '''
+    @staticmethod
+    def forward(ctx, input, nbit, alpha=None, offset=None):
+        ctx.alpha = alpha
+        ctx.offset = offset
+        scale = (2 ** nbit - 1) if alpha is None else (2 ** nbit - 1) / alpha
+        ctx.scale = scale
+        return torch.round(input * scale) / scale if offset is None \
+                else (torch.round(input * scale) + torch.round(offset)) / scale
+#         if alpha is None:
+#             scale = 2 ** nbit - 1
+#             ctx.scale = scale
+#             if offset is None:                
+#                 return torch.round(input * scale) / scale
+#             else:
+#                 return (torch.round(input * scale) + offset) / scale
+#         else:
+#             scale = (2 ** nbit - 1) / alpha
+#             if offset is None:                
+#                 return torch.round(input * scale) / scale
+#             else:
+#                 ctx.save_for_backward(input, scale)
+#                 return (torch.round(input * scale) + offset) / scale
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        if ctx.offset is None:
+            return grad_output, None, None, None
+        else:
+            return grad_output, None, None, torch.sum(grad_output) / ctx.scale
+
+
+def quantize(input, nbit, alpha=None, offset=None):
+    return Quantizer.apply(input, nbit, alpha, offset)
+
+# sign in dorefa-net for weights
+class ScaleSigner(Function):
+    '''
+    take a real value x
+    output sign(x) * E(|x|)
+    '''
+    @staticmethod
+    def forward(ctx, input):
+        return torch.sign(input) * torch.mean(torch.abs(input))
+#         return ((input>=0).float()*2-1) * torch.mean(torch.abs(input))
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output
+
+    
+def scale_sign(input):
+    return ScaleSigner.apply(input)
+
+def dorefa_w(w, nbit_w, *args, **kwargs):
+    if nbit_w == 1:
+        w = scale_sign(w)
+    else:
+        w = torch.tanh(w)
+        w = w / (2 * torch.max(torch.abs(w))) + 0.5
+        w = 2 * quantize(w, nbit_w) - 1
+    return w
+
+# dorefa quantize for activations
+def dorefa_a(input, nbit_a, *args, **kwargs):
+    return quantize(torch.clamp(input, 0, 1), nbit_a, *args, **kwargs)
+
+# PACT quantize for activations
+def pact_a(input, nbit_a, alpha, *args, **kwargs):
+    x = 0.5*(torch.abs(input)-torch.abs(input-alpha)+alpha)
+    return quantize(x, nbit_a, alpha, *args, **kwargs)
+
+
+class DynamicQConv(nn.Conv2d):
+    # general QuanConv for quantized conv
+    def __init__(self, in_channels, out_channels, kernel_size, quan_name_w, quan_name_a, nbit_w=32, nbit_a=32, has_offset=False, 
+                 stride=1, padding=0, dilation=1, groups=1, bias=True):
+        super(DynamicQConv, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.nbit_w = nbit_w
+        self.nbit_a = nbit_a
+        name_w_dict = {'dorefa': dorefa_w, 'pact': dorefa_w}
+        name_a_dict = {'dorefa': dorefa_a, 'pact': pact_a}
+        self.quan_w = name_w_dict[quan_name_w]
+        self.quan_a = name_a_dict[quan_name_a]
+        #self.alpha = None
+        if quan_name_a == 'pact':
+            self.alpha_a = nn.Parameter(torch.Tensor(1), requires_grad=True)
+        else:
+            self.register_parameter('alpha_a', None)
+        if quan_name_w == 'pact':
+            self.alpha_w = nn.Parameter(torch.Tensor(1), requires_grad=True)
+        else:
+            self.register_parameter('alpha_w', None)
+        if has_offset:
+            self.offset = nn.Parameter(torch.Tensor(1))
+        else:
+            self.register_parameter('offset', None)
+#         print(quan_name_w, quan_name_a, nbit_w, nbit_a)
+        
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+            
+        self.reset_custome_parameters()
+    
+    def reset_custome_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            nn.init.constant_(self.bias, 0)
+        if self.alpha_a is not None:
+            nn.init.constant_(self.alpha_a, 10)
+        if self.alpha_w is not None:
+            nn.init.constant_(self.alpha_w, 10)
+        if self.offset is not None:
+            nn.init.constant_(self.offset, 0)
+    
+    def forward(self, input, mask):
+        # 0-bit: identity mapping
+        if self.nbit_w == 0 or self.nbit_a == 0:
+            diff_channels = self.out_channels - self.in_channels
+            if self.stride == 2 or self.stride == (2, 2):
+                x = F.pad(input[:, :, ::2, ::2], (0, 0, 0, 0, diff_channels//2, diff_channels-diff_channels//2), 'constant', 0)
+                return x
+            else:
+                x = F.pad(input, (0, 0, 0, 0, diff_channels//2, diff_channels-diff_channels//2), 'constant', 0)
+                return x
+        # w quan
+        if self.nbit_w < 32:
+            # w = self.quan_w(self.weight, self.nbit_w, self.alpha_w, self.offset)
+            w0 = self.quan_w(self.weight, self.nbit_w-1, self.alpha_w, self.offset)
+            w1 = self.quan_w(self.weight, self.nbit_w, self.alpha_w, self.offset)
+            w2 = self.quan_w(self.weight, self.nbit_w+1, self.alpha_w, self.offset)
+        else:
+            w = self.weight
+        # a quan
+        if self.nbit_a < 32:
+            # x = self.quan_a(input, self.nbit_a, self.alpha_a)
+            x0 = self.quan_a(input, self.nbit_a-1, self.alpha_a)
+            x1 = self.quan_a(input, self.nbit_a, self.alpha_a)
+            x2 = self.quan_a(input, self.nbit_a+1, self.alpha_a)
+        else:
+            x = F.relu(input)
+        
+        x0 = F.conv2d(x0, w0, None, self.stride, self.padding, self.dilation, self.groups)
+        x1 = F.conv2d(x1, w1, None, self.stride, self.padding, self.dilation, self.groups)
+        x2 = F.conv2d(x2, w2, None, self.stride, self.padding, self.dilation, self.groups)
+        x = x0*mask[:,0].unsqueeze(1).unsqueeze(2).unsqueeze(3)+ \
+            x1*mask[:,1].unsqueeze(1).unsqueeze(2).unsqueeze(3)+ \
+            x2*mask[:,2].unsqueeze(1).unsqueeze(2).unsqueeze(3)
+        return x
+
+
+class QuanConv(nn.Conv2d):
+    # general QuanConv for quantized conv
+    def __init__(self, in_channels, out_channels, kernel_size, quan_name_w, quan_name_a, nbit_w=32, nbit_a=32, 
+                 stride=1, padding=0, dilation=1, groups=1, bias=True, has_offset=False):
+        super(QuanConv, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.nbit_w = nbit_w
+        self.nbit_a = nbit_a
+        name_w_dict = {'dorefa': dorefa_w, 'pact': dorefa_w}
+        name_a_dict = {'dorefa': dorefa_a, 'pact': pact_a}
+        self.quan_w = name_w_dict[quan_name_w]
+        self.quan_a = name_a_dict[quan_name_a]
+        #self.alpha = None
+        if quan_name_a == 'pact':
+            self.alpha_a = nn.Parameter(torch.Tensor(1), requires_grad=True)
+        else:
+            self.register_parameter('alpha_a', None)
+        if quan_name_w == 'pact':
+            self.alpha_w = nn.Parameter(torch.Tensor(1), requires_grad=True)
+        else:
+            self.register_parameter('alpha_w', None)
+        if has_offset:
+            self.offset = nn.Parameter(torch.Tensor(1))
+        else:
+            self.register_parameter('offset', None)
+#         print(quan_name_w, quan_name_a, nbit_w, nbit_a)
+        
+        if bias:
+            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+            
+        self.reset_custome_parameters()
+    
+    def reset_custome_parameters(self):
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        if self.bias is not None:
+            nn.init.constant_(self.bias, 0)
+        if self.alpha_a is not None:
+            nn.init.constant_(self.alpha_a, 10)
+        if self.alpha_w is not None:
+            nn.init.constant_(self.alpha_w, 10)
+        if self.offset is not None:
+            nn.init.constant_(self.offset, 0)
+    
+    def forward(self, input):
+        # 0-bit: identity mapping
+        if self.nbit_w == 0 or self.nbit_a == 0:
+            diff_channels = self.out_channels - self.in_channels
+            if self.stride == 2 or self.stride == (2, 2):
+                x = F.pad(input[:, :, ::2, ::2], (0, 0, 0, 0, diff_channels//2, diff_channels-diff_channels//2), 'constant', 0)
+                return x
+            else:
+                x = F.pad(input, (0, 0, 0, 0, diff_channels//2, diff_channels-diff_channels//2), 'constant', 0)
+                return x
+        # w quan
+        if self.nbit_w < 32:
+            w = self.quan_w(self.weight, self.nbit_w, self.alpha_w, self.offset)
+        else:
+            w = self.weight
+        # a quan
+        if self.nbit_a < 32:
+            x = self.quan_a(input, self.nbit_a, self.alpha_a)
+        else:
+            x = F.relu(input)
+        
+        x = F.conv2d(x, w, None, self.stride, self.padding, self.dilation, self.groups)
+        return x
