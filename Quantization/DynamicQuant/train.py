62504987da7f55f7e118616ae58108f99c9a7d0e
liu-zhenhua <1471910080@qq.com>
2022-08-29 16:14:38 +0800

    Update train.py

--- Quantization/DynamicQuant/train.py
+++ Quantization/DynamicQuant/train.py
@@ -77,7 +77,7 @@ parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
                     help='url used to set up distributed training')
 parser.add_argument('--dist-backend', default='nccl', type=str,
                     help='distributed backend')
-parser.add_argument('--seed', default=None, type=int,
+parser.add_argument('--seed', default=0, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--gpu', default=None, type=int,
                     help='GPU id to use.')
